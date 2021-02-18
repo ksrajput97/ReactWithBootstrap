0226ebddd6706b819365763810dee8137ad25c48
krishan singh <krishansingh9779@gmail.com>
2021-02-18 20:41:10 +0530

    app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
+    <div>
     <h2>Bootstrap-project </h2>
+    <h3>This is the small changes</h3>
+    </div>
   );
 }
 
